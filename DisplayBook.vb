af2c562e14f98610044a40961a4ac64f3960df7d
LanceParantar <20180014283@my.xu.edu.ph>
2021-03-19 12:40:24 +0800

    test

---
+++ DisplayBook.vb
@@ -0,0 +1,7 @@
+﻿Module DisplayBook
+
+    Public Sub updateBookVisuals()
+        Dim usCntl As New bookDp
+        Main.FlowLayoutPanel1.Controls.Add(usnCntl)
+    End Sub
+End Module
---
+++ bookDp.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class bookDp
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Btitle = New System.Windows.Forms.Label()
+        Me.BStatus = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.img = New System.Windows.Forms.PictureBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Blanguage = New System.Windows.Forms.Label()
+        Me.Bpublisher = New System.Windows.Forms.Label()
+        CType(Me.img, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Maiandra GD", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(292, 177)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(78, 20)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Publisher:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Maiandra GD", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(292, 157)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(88, 20)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Language: "
+        '
+        'Btitle
+        '
+        Me.Btitle.AutoSize = True
+        Me.Btitle.Font = New System.Drawing.Font("Maiandra GD", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(31, Byte), Integer), CType(CType(127, Byte), Integer), CType(CType(146, Byte), Integer))
+        Me.Btitle.Location = New System.Drawing.Point(290, 52)
+        Me.Btitle.Name = "Btitle"
+        Me.Btitle.Size = New System.Drawing.Size(742, 102)
+        Me.Btitle.TabIndex = 9
+        Me.Btitle.Text = "Star wars: The rise of Skywalker the visual dictionary (2019)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'BStatus
+        '
+        Me.BStatus.AutoSize = True
+        Me.BStatus.Font = New System.Drawing.Font("Sitka Display", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BStatus.ForeColor = System.Drawing.Color.Lime
+        Me.BStatus.Location = New System.Drawing.Point(139, 284)
+        Me.BStatus.Name = "BStatus"
+        Me.BStatus.Size = New System.Drawing.Size(84, 29)
+        Me.BStatus.TabIndex = 8
+        Me.BStatus.Text = "Available"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Sitka Display", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(78, 284)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 29)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Status: "
+        '
+        'img
+        '
+        Me.img.Image = Global.LibraryManagementSystem.My.Resources.Resources.pic_1
+        Me.img.Location = New System.Drawing.Point(29, 16)
+        Me.img.Name = "img"
+        Me.img.Size = New System.Drawing.Size(233, 251)
+        Me.img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.img.TabIndex = 6
+        Me.img.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Maiandra GD", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(31, Byte), Integer), CType(CType(127, Byte), Integer), CType(CType(146, Byte), Integer))
+        Me.Label6.Location = New System.Drawing.Point(290, 100)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(150, 34)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Adcock F.E"
+        '
+        'Blanguage
+        '
+        Me.Blanguage.AutoSize = True
+        Me.Blanguage.Font = New System.Drawing.Font("Maiandra GD", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Blanguage.Location = New System.Drawing.Point(377, 157)
+        Me.Blanguage.Name = "Blanguage"
+        Me.Blanguage.Size = New System.Drawing.Size(28, 20)
+        Me.Blanguage.TabIndex = 10
+        Me.Blanguage.Text = "En"
+        '
+        'Bpublisher
+        '
+        Me.Bpublisher.AutoSize = True
+        Me.Bpublisher.Font = New System.Drawing.Font("Maiandra GD", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bpublisher.Location = New System.Drawing.Point(376, 177)
+        Me.Bpublisher.Name = "Bpublisher"
+        Me.Bpublisher.Size = New System.Drawing.Size(392, 20)
+        Me.Bpublisher.TabIndex = 11
+        Me.Bpublisher.Text = "Solon, Ohio : Findaway World, LLC, [2020] ℗2020"
+        '
+        'bookDp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Bpublisher)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Blanguage)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Btitle)
+        Me.Controls.Add(Me.BStatus)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.img)
+        Me.Name = "bookDp"
+        Me.Size = New System.Drawing.Size(1079, 364)
+        CType(Me.img, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Btitle As Label
+    Friend WithEvents BStatus As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents img As PictureBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Blanguage As Label
+    Friend WithEvents Bpublisher As Label
+End Class
