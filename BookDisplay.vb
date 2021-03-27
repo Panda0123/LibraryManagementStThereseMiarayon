287f53ba3a24fad4d3f535d780f07de21569110b
LanceParantar <20180014283@my.xu.edu.ph>
2021-03-27 13:38:58 +0800

    Update v1
    merge branch1 to main

--- BookDisplay.vb
+++ BookDisplay.vb
@@ -1,46 +1,27 @@
-﻿Module BookDisplay
+﻿Public Class bookDisplay
+    Private Title As String
+    Private Author As List(Of String)
+    Private img As Image
+    Private Langauge As String
+    Private Publisher As String
+    Private Status As String
 
-    Public Sub load(ByRef books As List(Of Book))
-
-
-    End Sub
-
-
-
-
-
-    Public Sub updateBookVisuals()
-
-
-        Dim rows As Integer
-        Dim p As Integer
-
-        rows = 5
-
-        Main.books.Add(New Book)
-
-
-
-        Dim pal As Integer = 0
-
-
-
-
-
-        Main.DisplayBookPanel.Controls.Add(Main.books.Item(0))
-
-
-
-
-
-
-
-
+    Private Sub RectangleShape1_Click(sender As Object, e As EventArgs) Handles RectangleShape1.Click
 
     End Sub
 
+    '  Public Sub New(Title As String, author As List(Of String), img As Image, Language As String, Publisher As String, Status As String)
 
+    ' This call is required by the designer.
+    '     InitializeComponent()
+    ' Me.Title = Title
+    '     Me.Author = author
+    'Me.img = img
+    ' Me.Langauge = Langauge
+    '   Me.Publisher = Publisher
+    'Me.Status = Status
+    ' Add any initialization after the InitializeComponent() call.
 
+    ' End Sub
 
-
-End Module
+End Class
